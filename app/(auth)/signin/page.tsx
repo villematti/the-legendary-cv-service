493059f0163d071eb55046986951103ee331034e
RichardT <chardiecode@gmail.com>
2024-09-05 14:40:43 +0800

    Initialize project

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div>Signin page</div>;
+};
+
+export default SignIn;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <div>page</div>;
+};
+
+export default SignUp;
